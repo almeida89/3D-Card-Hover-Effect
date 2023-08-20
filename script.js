344a89c3d74d5d8d01978e9c0fe8afcc85c185b7
Fabio <fabio_almeida89@outlook.com>
2023-08-19 23:42:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//Animação para acontecer o movimento
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+//Itens
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+//Evento de animação em movimento
+container.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+//Animar dentro
+container.addEventListener("mouseenter", (e) => {
+    card.style.transition = "none";
+
+//Pop-out
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-25deg)";
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(75px)"; 
+});
+
+//Animar fora
+container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+//PopBack
+    title.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
+});
